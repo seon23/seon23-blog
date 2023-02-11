6e0c2cfbdd36d744e45e7c4e9d99ee2a7a8d1520
seon23 <govkfl100@gmail.com>
2023-02-12 07:21:37 +0900

    :sparkles: Gatsby Tutorial Part4 완료

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title: "Seon's blog",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
