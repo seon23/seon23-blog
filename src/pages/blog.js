24d241bd5a4bbb0427132d33265a1f2a280d3a07
seon23 <govkfl100@gmail.com>
2023-02-20 19:31:39 +0900

    :sparkles: Blog 페이지에 각 게시물의 날짜, 프리뷰 추가

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,33 +1,38 @@
-import * as React from 'react'
-import { graphql} from 'gatsby'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import * as React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-        <ul>
-            {
-                data.allFile.nodes.map(node => (
-                    <li key={node.name}>
-                        {node.name}
-                    </li>
-                ))
-            }
-        </ul>
+      {
+        data.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+                <h2>{node.frontmatter.title}</h2>
+                <p>Posted: {node.frontmatter.date}</p>
+                <p>{node.excerpt}</p>
+            </article>
+        ))
+      }
     </Layout>
-  )
-}
+  );
+};
 export const query = graphql`
-    query {
-        allFile {
-            nodes {
-                name
-            }
+  query {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
         }
+        id
+        excerpt
+      }
     }
-`
+  }
+`;
 
-export const Head = ({ data }) => <Seo title="My Blog Posts" />
+export const Head = () => <Seo title="My Blog Posts" />;
 
-export default BlogPage
+export default BlogPage;
