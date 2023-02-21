b257a34d5260fc9c6b04423514f36205bd882520
seon23 <govkfl100@gmail.com>
2023-02-21 21:17:57 +0900

    :truck: 파일 이동 후 import 변경

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,20 +1,18 @@
 import * as React from "react";
 import { graphql } from "gatsby";
-import Layout from "../components/layout";
-import Seo from "../components/seo";
+import Layout from "../../components/layout";
+import Seo from "../../components/seo";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      {
-        data.allMdx.nodes.map((node) => (
-            <article key={node.id}>
-                <h2>{node.frontmatter.title}</h2>
-                <p>Posted: {node.frontmatter.date}</p>
-                <p>{node.excerpt}</p>
-            </article>
-        ))
-      }
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
     </Layout>
   );
 };
