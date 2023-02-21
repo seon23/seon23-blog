42082295c6d241390ab1a88a568902eec25fb1ff
seon23 <govkfl100@gmail.com>
2023-02-22 05:57:27 +0900

    ✨ Blog 페이지에 각 게시물의 링크 삽입

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,16 +1,19 @@
 import * as React from "react";
-import { graphql } from "gatsby";
+import {Link, graphql} from "gatsby";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 
-const BlogPage = ({ data }) => {
+const BlogPage = ({data}) => {
   return (
-    <Layout pageTitle="My Blog Posts">
+    <Layout pageTitle='My Blog Posts'>
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <h2>
+            <Link to={`/blog/${node.frontmatter.slug}`}>
+              {node.frontmatter.title}
+            </Link>
+          </h2>
           <p>Posted: {node.frontmatter.date}</p>
-          <p>{node.excerpt}</p>
         </article>
       ))}
     </Layout>
@@ -18,19 +21,19 @@ const BlogPage = ({ data }) => {
 };
 export const query = graphql`
   query {
-    allMdx(sort: { frontmatter: { date: DESC } }) {
+    allMdx(sort: {frontmatter: {date: DESC}}) {
       nodes {
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
-        excerpt
       }
     }
   }
 `;
 
-export const Head = () => <Seo title="My Blog Posts" />;
+export const Head = () => <Seo title='My Blog Posts' />;
 
 export default BlogPage;
